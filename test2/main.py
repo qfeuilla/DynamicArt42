1b824355794c319e8be91e05002d916731d90072
Quentin FEUILLADE--MONTIXI <CrackingProg42@users.noreply.github.com>
2020-03-29 20:15:47 +0200

    correct error

--- test2/main.py
+++ test2/main.py
@@ -1,6 +1,6 @@
 from transfer import Transfer
 
-test = Transfer(20, "./", "./style/style1.jpg", 1e-3, 1, 5, 1e-2)
+test = Transfer(20, "./", "./style/style1.jpg", 2, 1e-3, 1, 5, 1e-2)
 # test.load_weight('./model/save_style1.pth')
 # test.predict("./test/land.jpg")
 test.train()
