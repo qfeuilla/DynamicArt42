a88b5ad19407f0632c285755b655fc569ad3eb3d
Quentin FEUILLADE--MONTIXI <CrackingProg42@users.noreply.github.com>
2020-04-02 14:02:08 +0200

    update values

--- test2/main.py
+++ test2/main.py
@@ -1,6 +1,6 @@
 from transfer import Transfer
 
-test = Transfer(20, "./", "./style/style1.jpg", 4, 1e-3, 1e4, 1e12)
+test = Transfer(20, "./", "./style/style1.jpg", 4, 1e-3, 1e4, 1e10)
 # test.load_weight('./model/net_save_epochs_2.pth')
 # test.predict("./test/emma.jpg")
 # test.live_transfer()
